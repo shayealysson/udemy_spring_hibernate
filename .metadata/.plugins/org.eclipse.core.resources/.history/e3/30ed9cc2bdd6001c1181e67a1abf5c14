7cc2b5c2f0620c43f5c38cb8316deb91d03ea34d
Shaye Baisac <shayebaisac@gmail.com>
2022-05-18 23:20:42 +0800

    Bean Scope: PROOF that default bean scope is SINGLETON

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e3/30ed9cc2bdd6001c1181e67a1abf5c14
@@ -0,0 +1,18 @@
+package com.shayealysson.beanscopedemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		// load the spring config file
+		ClassPathXmlApplicationContext context = new 
+				ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		// retrieve bean from spring container
+		
+		//
+
+	}
+
+}
--- spring-demo-one/src/com/shayealysson/beanscopedemo/BeanScopeDemoApp.java
+++ spring-demo-one/src/com/shayealysson/beanscopedemo/BeanScopeDemoApp.java
@@ -2,6 +2,8 @@ package com.shayealysson.beanscopedemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.shayealysson.springdemo.Coach;
+
 public class BeanScopeDemoApp {
 
 	public static void main(String[] args) {
@@ -10,8 +12,18 @@ public class BeanScopeDemoApp {
 				ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
 		
 		// retrieve bean from spring container
+		Coach coachA = context.getBean("myCoachService", Coach.class);
+		Coach coachB = context.getBean("myCoachService", Coach.class);
 		
-		//
+		// Check if they are the same bean
+		boolean result = (coachA == coachB);
+		System.out.println("\nSame object? " + result);
+		System.out.println("\nMemory location coachA: " + coachA);
+		System.out.println("\nMemory location coachB: " + coachB);
+		
+		
+		// close context
+		context.close();
 
 	}
 
