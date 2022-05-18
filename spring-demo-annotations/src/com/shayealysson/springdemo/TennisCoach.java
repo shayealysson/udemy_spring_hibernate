2f3626e204052606fdf2e46872718e288b42834f
Shaye Baisac <shayebaisac@gmail.com>
2022-05-19 00:59:23 +0800

    Spring Annotations: use defaul bean ID

--- spring-demo-annotations/src/com/shayealysson/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/shayealysson/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.shayealysson.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("tennisCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
