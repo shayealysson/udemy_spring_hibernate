e55e33d16435280a7a55a8724331dd95d074988f
Shaye Baisac <shayebaisac@gmail.com>
2022-05-19 16:24:06 +0800

    @Scope Annotation: prototype

--- spring-demo-annotations/src/com/shayealysson/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/shayealysson/springdemo/TennisCoach.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("singleton")
+@Scope("prototype")
 public class TennisCoach implements Coach {
 	
 	@Autowired
